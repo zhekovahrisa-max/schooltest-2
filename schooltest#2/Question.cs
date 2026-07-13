b130dc6cdc0551a496eefafd16b3dee747ce4616
Hrisa Zhekova <zhekovahrisa@gmail.com>
2026-07-13 21:52:59 +0300

    Add files via upload
    chistiq fail

---
+++ schooltest#2/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schooltest_2
+{
+    
+        public class Question
+        {
+            public int Id { get; set; }
+            public string QuestionText { get; set; }
+            public List<string> Answers { get; set; }
+            public int CorrectAnswerId { get; set; } 
+            public Question(int id, string text, List<string> answers, int correctAnswerId)
+            {
+                this.Id = id;
+                this.QuestionText = text;
+                this.Answers = answers;
+                this.CorrectAnswerId = correctAnswerId;
+            }
+        }
+    
+}
